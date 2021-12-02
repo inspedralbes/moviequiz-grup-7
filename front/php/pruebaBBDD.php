abf2c76bc9e482f3cce684f8c44bae7b9ca7de25
a18kevhoylor <a18kevhoylor@inspedralbes.cat>
2021-12-02 13:29:30 +0100

    Creacion con PHP del archivo JSON con contenido de la BBDD

---
+++ front/php/pruebaBBDD.php
@@ -0,0 +1,34 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "proyecto";
+
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+$db = mysqli_select_db( $conn, $db_name);
+
+$consulta = "SELECT * FROM peliculas";
+
+$resultado = mysqli_query( $conn, $consulta );
+
+$array = [];
+
+
+while ($columna = mysqli_fetch_array( $resultado )) {
+
+    array_push($array, "{'id': '$columna[id]' , 'titulo':  '$columna[titulo]' , 'imagen': '$columna[imagen]'}");
+
+}
+$myJSON = json_encode($array);
+echo $myJSON;
+$file = "./../json/peliculas.json";
+file_put_contents($file, $myJSON);
+
+mysqli_close( $conn );
+
+
+
+
