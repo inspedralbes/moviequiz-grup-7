8b7fb80c137796590ed6775b1fcaf44ac56d5cfb
a18marcastru <a17marlamver@inspedralbes.cat>
2021-12-02 12:25:22 +0100

    Registrarse

--- back/sql/create.sql
+++ back/sql/create.sql
@@ -3,13 +3,12 @@ CREATE DATABASE IF NOT EXISTS Proyecto;
 CREATE TABLE IF NOT EXISTS Proyecto.peliculas (
 
     id VARCHAR(15) PRIMARY KEY,
-    titulo VARCHAR(30);
-    imagen VARCHAR(255);
-    
+    titulo VARCHAR(30),
+    imagen VARCHAR(255)
 );
 CREATE TABLE IF NOT EXISTS Proyecto.usuarios (
 
-    id VARCHAR(15) AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(30);
-    contrasena VARCHAR(30);
+    id INT(15) AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(30),
+    contrasena VARCHAR(30)
 )
