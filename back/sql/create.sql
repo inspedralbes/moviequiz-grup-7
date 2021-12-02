2c14d03d6c013b71936794b67447e08f5192949e
a18marcastru <a17marlamver@inspedralbes.cat>
2021-12-02 11:49:33 +0100

    BBDD

---
+++ back/sql/create.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS Proyecto;
+
+CREATE TABLE IF NOT EXISTS Proyecto.peliculas (
+
+    id VARCHAR(15) PRIMARY KEY,
+    titulo VARCHAR(30);
+    imagen VARCHAR(255);
+    
+);
+CREATE TABLE IF NOT EXISTS Proyecto.usuarios (
+
+    id VARCHAR(15) AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(30);
+    contrasena VARCHAR(30);
+)
