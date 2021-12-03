bd701aadf71e498445b0eca3efe9f7b45ba42e01
a18kevhoylor <a18kevhoylor@inspedralbes.cat>
2021-12-03 13:33:17 +0100

    Intento mostrar con json en pelis.html

--- front/js/ejemplo.js
+++ front/js/ejemplo.js
@@ -1,18 +1,21 @@
-    function buscar(){
-        var peli = document.getElementById("cercar").value;
-        var div = document.getElementById("pelis");
-        fetch("http://www.omdbapi.com/?s=" +peli+ "&apikey=ae8cf42f")
-        .then(response => response.json())
-        .then(data => {
-            for(let i =0; i< data.Search.length; i++){
-                let h2 = document.createElement("h2");
-                let img = document.createElement("img");
-                h2.innerHTML = data.Search[i].Title;
-                img.src = data.Search[i].Poster;
+fetch(`./../json/peliculas.json`).then(function(res) {
+            return res.json();
 
-                div.append(h2, img);
-            }
+        }).then(function(data) {
             console.log(data);
+
+            let pelis = "";
+            for(i=0; i<10; i++){
+                datos = data[i];
+                pelis += `<div>
+                <h2>${datos.titulo}</h2>
+                <img src="${datos.imagen}"></img>
+                </div>`;
+            }
+            document.getElementById("imprimir").innerHTML=pelis;
+        }).catch(function() {
+            console.log("problem!");
         });
-        }
+
+
         
