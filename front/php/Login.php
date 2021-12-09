0ed4d6436e3b8c94614efcdd40322b2ae9ceab38
a18marcastru <a17marlamver@inspedralbes.cat>
2021-12-09 10:15:36 +0100

    "login"

--- front/php/Login.php
+++ front/php/Login.php
@@ -1,1 +1,20 @@
 <?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "proyecto";
+
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+$db = mysqli_select_db($conn, $db_name);
+
+$correo = $_REQUEST['correo'];
+$contrasena = $_REQUEST['passwd'];
+
+$consulta = "SELECT * from usuarios WHERE $correo = correo AND $contrasena = contrasena";
+
+
+mysqli_close( $conn );
+header("Location: ./../html/Pelis.html");
