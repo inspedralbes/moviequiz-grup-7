29fa28e5ca387e95489351500e40f4861160a99b
a18marcastru <a17marlamver@inspedralbes.cat>
2021-12-13 14:12:14 +0100

    Errores del login

--- front/php/Login.php
+++ front/php/Login.php
@@ -4,24 +4,43 @@ include ("Connect.php");
 $correo = $_POST['correo'];
 $contrasena = $_POST['passwd'];
 
-$correo = stripcslashes($correo);
-$contrasena = stripcslashes($contrasena);
-$correo = mysqli_real_escape_string($conn, $correo);
-$contrasena = mysqli_real_escape_string($conn, $contrasena);
-
-$consulta = "SELECT * from usuarios WHERE correo = '$correo' AND contrasena = $contrasena";
-$result = mysqli_query($conn, $consulta);
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$count = mysqli_num_rows($result);
-
-if($count == 1){
-    $arr = array('exito' => true, 'correo' => "$correo", 'contrasena' => "$contrasena", 'imagen' => 'https://randomuser.me/api/portraits/men/23.jpg');;
+if($correo == NULL){
+    if($contrasena == NULL){
+        $arr = array ('exito' =>false, 'mensaje' =>"Correo y contrasena no definidos");
+        $myJSON = json_encode($arr);
+        echo $myJSON;
+    }
+    else {
+        $arr = array('exito' => false, 'mensaje' => "Correo no definido");
+        $myJSON = json_encode($arr);
+        echo $myJSON;
+    }
+}
+else if($contrasena == NULL){
+    $arr = array ('exito' =>false, 'mensaje' =>"Contrasena no definida");
+    $myJSON = json_encode($arr);
+    echo $myJSON;
 }
 else{
-    $arr = array ('exito' =>false);
-}
-$myJSON = json_encode($arr);
-echo $myJSON;
+    $correo = stripcslashes($correo);
+    $contrasena = stripcslashes($contrasena);
+    $correo = mysqli_real_escape_string($conn, $correo);
+    $contrasena = mysqli_real_escape_string($conn, $contrasena);
 
-mysqli_close($conn);
+    $consulta = "SELECT * from usuarios WHERE correo = '$correo' AND contrasena = $contrasena";
+    $result = mysqli_query($conn, $consulta);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
+
+    if($count == 1){
+        $arr = array('exito' => true, 'correo' => "$correo", 'contrasena' => "$contrasena", 'imagen' => 'https://randomuser.me/api/portraits/men/23.jpg');;
+    }
+    else{
+        $arr = array ('exito' =>false);
+    }
+    $myJSON = json_encode($arr);
+    echo $myJSON;
+
+    mysqli_close($conn);
+}
 ?>
