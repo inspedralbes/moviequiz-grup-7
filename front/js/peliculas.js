952ef7303ea9520cf97a755f78da10ffcfb4605e
jost243 <92725911+jost243@users.noreply.github.com>
2021-12-13 09:08:46 +0100

    Create peliculas.js

---
+++ front/js/peliculas.js
@@ -0,0 +1,57 @@
+fetch(`./../json/peliculas.json`).then(function(res) {
+            return res.json();
+
+        }).then(function(data) {
+            
+            console.log(data);
+
+            let pelis = "";
+            for(i=0; i<data.length; i++){
+                datos = data[i];
+                pelis += `<div>
+                <h2>${datos.titulo}</h2>
+                <img src="${datos.imagen}"></img>
+                </div>`;
+            }
+            document.getElementById("pelis").innerHTML=pelis;
+    //    }).catch(function() {
+    //        console.log("problem!");
+        });
+
+
+function buscando(){
+        let pelicula=document.getElementById("buscar").value;
+        let PeliculasPrincipales = document.getElementById("pelis");
+        PeliculasPrincipales.style.display="none";
+        fetch(`https://www.omdbapi.com/?apikey=9dbe2ca&s=${pelicula}`).then(function(res) {
+            return res.json();
+        }).then(function(data) {
+            console.log(data);
+            let peliculas = "";
+            for(i=0; i<8; i++) {
+                datos = data.Search[i];
+                peliculas += ` <div class="row center">
+                                            <div class="col s6 m3 l3">
+                                                <div class="card">
+                                                    <div class="card-image">
+                                                    <img width="100px" src="${datos.Poster}"></img>
+                                                    <h3>${datos.Title}</h3>
+                                                    <a class="waves-effect waves-light btn">button</a>
+                                                    </div>
+                                                </div>
+                                        </div>`;
+            }
+            document.getElementById("peliculas").innerHTML=peliculas;
+            //document.getElementsByClassName("profile-image")[0].setAttribute("src",dadesUsr.picture.thumbnail);
+            //}).catch(function() {
+            //console.log("problem!");
+        });
+    }
+
+
+
+
+
+
+
+
