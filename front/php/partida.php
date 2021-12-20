0b62dfc0e8df2433a89b7ec04abb9b33c23a7f58
a18marcastru <a17marlamver@inspedralbes.cat>
2021-12-20 08:21:41 +0100

    partida

--- front/php/partida.php
+++ front/php/partida.php
@@ -2,14 +2,32 @@
 
 include ("Connect.php");
 
-$array = [-15 , -10 , -5 , -2  , 2 , +5 , +10 ,+15];
-
-$sql = "SELECT ano from peliculas";
+$sumasrestas = [-15 , -10 , -5 , -2  , 2 , +5 , +10 ,+15];
+$random = array_rand($sumasrestas, 3);
+$sql = "SELECT id, titulo, imagen, ano from peliculas ORDER BY RAND() LIMIT 5 ";
 $result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$count = mysqli_num_rows($result);
+for($i =0; $i<$result->num_rows;$i++){
+    $rows[$i]=$result->fetch_assoc();
+}
 
-$arr = ($row);
-$myJSON = json_encode($arr);
+$random = range(1,4);
+shuffle($random);
+$pos = array_slice($random, 0, 4);
+
+for($i=0; $i<count($rows);$i++){
+
+    $anys[1]=$rows[$i]['ano'];
+    $anys[2]=$rows[$i]['ano']+$sumasrestas[$random[0]];
+    $anys[3]=$rows[$i]['ano']+$sumasrestas[$random[1]];
+    $anys[4]=$rows[$i]['ano']+$sumasrestas[$random[2]];
+
+    $nouArray[$i]['id'] = $rows[$i]['id'];
+    $nouArray[$i]['titulo'] = $rows[$i]['titulo'];
+    $nouArray[$i]['any1'] = $anys[$pos[0]];
+    $nouArray[$i]['any2 '] = $anys[$pos[1]];
+    $nouArray[$i]['any3 '] = $anys[$pos[2]];
+    $nouArray[$i]['any4 '] = $anys[$pos[3]];
+}
+$peliculas['peliculas'] = $nouArray;
+$myJSON = json_encode($peliculas);
 echo $myJSON;
-
