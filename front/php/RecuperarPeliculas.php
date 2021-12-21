22fa99f93403ad5c6acc0b2fb38ab3f90e677c9c
jost243 <a20joslomdur@inspedralbes.cat>
2021-12-21 23:42:27 +0100

    Update RecuperarPeliculas.php

--- front/php/RecuperarPeliculas.php
+++ front/php/RecuperarPeliculas.php
@@ -2,21 +2,6 @@
 
 include ("Connect.php");
 
-/*session_start();
-if (isset($_SESSION['usuario'])) {
-    $user = $_SESSION['usuario'];
-}
-$comentario = $_REQUEST['comentario'];
-$favorito = $_REQUEST['favorito'];
-$valoracion= $_REQUEST['valoracion'];
-*/
-
-/*
-$comprobar = "SELECT * FROM peliculas WHERE id = '$imdbId'";
-$result = mysqli_query($conn, $comprobar);
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-$count = mysqli_num_rows($result);*/
-
 $consulta = "SELECT * FROM peliculas";
 $resultado =  mysqli_query( $conn, $consulta );
 $array = [];
@@ -27,8 +12,6 @@ $array = [];
     }
     $myJSON = json_encode($array);
     echo ($myJSON);
-  //  $file = "./../json/usuarios.json";
- //   file_put_contents($file, $myJSON);
 
 mysqli_close($conn);
 
