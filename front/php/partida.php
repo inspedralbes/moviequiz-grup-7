aecd7a82214bd1c851c3dc60beb72c69b407ea30
a18marcastru <a17marlamver@inspedralbes.cat>
2021-12-21 14:13:21 +0100

    cambio pequeño

--- front/php/partida.php
+++ front/php/partida.php
@@ -34,6 +34,6 @@ $result2 = mysqli_query($conn, $id_partida);
 $row = mysqli_fetch_array($result2, MYSQLI_ASSOC);
 $peliculas['peliculas'] = $nouArray;
 $resultado = array_merge($row, $peliculas);
-$json_string = json_encode($resultado);
-$file = './../json/partida.json';
-file_put_contents($file, $json_string);
+echo json_encode($resultado);
+/*$file = './../json/partida.json';
+file_put_contents($file, $json_string);*/
